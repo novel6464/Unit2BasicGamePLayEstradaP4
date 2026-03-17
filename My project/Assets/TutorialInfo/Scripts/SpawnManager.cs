22ef982a982588837acf4e57c352db0b62350fc0
novel6464 <764297@student.dusd.net>
2026-03-17 11:21:48 -0700

    qweqwqwe
    qweqweqwe

--- My project/Assets/TutorialInfo/Scripts/SpawnManager.cs
+++ My project/Assets/TutorialInfo/Scripts/SpawnManager.cs
@@ -1,27 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalPrefabs; // Array of animal prefabs that can be spawned
-    private float spawnRangeX = 20.0f; // Range for spawning animals on the x-axis
-    private float spawnPosZ = 20.0f; // Position for spawning animals on the z-axis
+    // Array to hold the animal prefabs
+    public GameObject[] animalPrefabs;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Variables for the spawn range and position
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        // For Lesson 2.3, we typically use the 'S' key in Update, 
+        // but some versions of the tutorial introduce InvokeRepeating here.
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) // Check if the 'S' key is pressed
+        // Step 2 & 3: Spawn a random animal when 'S' is pressed
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            SpawnRandomAnimal();
+        }
+    }
 
+    // Step 4: Create a dedicated method for spawning
+    void SpawnRandomAnimal()
+    {
+        // Randomly generate animal index and spawn position
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }   
+        // Instantiate the selected animal prefab at the random position
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
+
